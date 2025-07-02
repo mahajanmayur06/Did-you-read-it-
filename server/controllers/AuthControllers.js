897377efa11112a71546d651f13b0a01c49f5468
Mayur Mahajan <mah.mayur06@gmail.com>
2025-07-03 00:08:06 +0530

    backend + sockets

--- server/controllers/AuthControllers.js
+++ server/controllers/AuthControllers.js
@@ -3,53 +3,53 @@ export const login = async (req, res) => {
 
 }
 export const register = async (req, res) => {
-    const {email,username}=req.body;
-    try{
-        const findExisitingUser=await UserModel.findOne({
-            username:username
+    const { email, username } = req.body;
+    try {
+        const findExisitingUser = await UserModel.findOne({
+            username: username
         });
-        if(findExisitingUser){
-            return res.status(400).json({message:"User already exists"});
+        if (findExisitingUser) {
+            return res.status(400).json({ message: "User already exists" });
         }
-        const user=await UserModel.create({
+        const user = await UserModel.create({
             email,
             username
         });
         res.status(201).json(user);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
-        res.status(500).json({message:"Internal Server Error"});
-}
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
 export const logout = async (req, res) => {
 
 }
-export const checkUsername=async(req,res)=>{
- try{
-   const {username}=req.params;
+export const checkUsername = async (req, res) => {
+    try {
+        const { username } = req.params;
 
-   const user =await UserModel.findOne({
-       username:username
-   });
-    if(user){
-         res.status(200).json({message:"User already exists"});
+        const user = await UserModel.findOne({
+            username: username
+        });
+        if (user) {
+            res.status(200).json({ message: "User already exists" });
+        }
+        else {
+            res.status(200).json({ message: "User doesnt exist" });
+        }
     }
-    else{
-         res.status(200).json({message:"User doesnt exist"});
+    catch (err) {
+        console.log(err);
     }
- }
- catch(err){
-    console.log(err);
- }
 }
-export const getAllUsers=async(req,res)=>{
-    try{
-        const users=await UserModel.find();
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await UserModel.find();
         res.status(200).json(users);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
-        res.status(500).json({message:"Internal Server Error"});
+        res.status(500).json({ message: "Internal Server Error" });
     }
-    }
+}
--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { checkUsername, getAllUsers, login, register } from '../controllers/AuthControllers.js';
-const router=express.Router();
-router.post('/register',register);
+
+const router = express.Router();
+
+router.post('/register', register);
 router.post('/login', login);
 // router.post('/logout');
-router.get('/check-username/:username',checkUsername);
-router.get('/all-users',getAllUsers);
+router.get('/check-username/:username', checkUsername);
+router.get('/all-users', getAllUsers);
+
 export default router;
